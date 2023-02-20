886c07d7415d1958637b07342577e9878a6aa6eb
RounakRajSingh <raunakrajsingh620@gmail.com>
2023-02-20 20:06:31 +0530

    Linkedin-Clone

--- src/Header.js
+++ src/Header.js
@@ -26,6 +26,7 @@ function Header() {
             <HeaderOption  Icon={BusinessCenterIcon} title='Jobs'/>
             <HeaderOption  Icon={ChatIcon} title='Messaging'/>
             <HeaderOption  Icon={NotificationsIcon} title='Notifications'/>
+            <HeaderOption avatar="https://media.licdn.com/dms/image/D5603AQEw_vOIyBs0gQ/profile-displayphoto-shrink_800_800/0/1669486040370?e=1682553600&v=beta&t=TunEkkMpuzc3o5-NDgm01TlLhc7_6L1lGZ4BtGsvP4U" title='me' />
 
             </div>
         </div>
--- src/HeaderOption.js
+++ src/HeaderOption.js
@@ -1,10 +1,12 @@
+import { Avatar } from '@mui/material';
 import React from 'react';
 import "./HeaderOption.css";
 
-function HeaderOption({Icon , title}) {
+function HeaderOption({ avatar, Icon , title}) {
   return (
     <div className="headerOption">
     {Icon && <Icon className='headerOption_icon'/>}
+    {avatar && <Avatar className="headerOption-icon" src={avatar} />}
     <h3 className="headerOption_title">{title}</h3>
       
     </div>
