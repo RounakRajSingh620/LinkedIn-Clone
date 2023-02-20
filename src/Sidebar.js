693ab349bcf83dcae67e8b6c9355c8b7f6043b04
RounakRajSingh <raunakrajsingh620@gmail.com>
2023-02-20 21:05:10 +0530

    Linkedin-Clone

---
+++ src/Sidebar.js
@@ -0,0 +1,19 @@
+import { Avatar } from '@mui/material'
+import React from 'react'
+import "./Sidebar.css"
+
+function Sidebar() {
+  return (
+    <div className="Sidebar">
+    <div className="sidebar_top">
+    <img src ="" alt=""/>
+    <Avatar />
+    </div>
+
+   
+      
+    </div>
+  )
+}
+
+export default Sidebar
