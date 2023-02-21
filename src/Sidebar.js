c92a152033efe73ba4c4523e2980ae711836d6f1
RounakRajSingh <raunakrajsingh620@gmail.com>
2023-02-21 22:27:59 +0530

    Linkedin-Clone

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -5,13 +5,29 @@ import "./Sidebar.css"
 function Sidebar() {
   return (
     <div className="Sidebar">
-    <div className="sidebar_top">
-    <img src ="" alt=""/>
-    <Avatar />
-    </div>
+      <div className="sidebar_top">
+        <img src="" alt="" />
+        <Avatar className='sidebar_avatar' />
+        <h2>Raunak Raj Singh</h2>
+        <h4>raunakrajsingh620@gmail.com</h4>
+      </div>
+      <div className="sidebar_stats">
+        <div className="sidebar_stat">
+          <p>Who viewed you</p>
+          <p className='sidebar__statNumber'>2,543</p>
+        </div>
+        <div className="sidebar_stat">
+          <p>Views on post</p>
+          <p className='sidebar__statNumber'>2,448</p>
+        </div>
+      </div>
 
-   
-      
+      <div className="sidebar_bottom">
+      <p>Recent</p>
+      </div>
+
+
+
     </div>
   )
 }
