9f1dec52d29f0f2bbb8fc073d00cb6460944397b
Rounak Raj Singh <97117309+RounakRajSingh620@users.noreply.github.com>
2023-03-06 20:53:42 +0530

    Linkedin-Clone

--- src/Post.js
+++ src/Post.js
@@ -1,19 +1,31 @@
+
 import { Avatar } from '@mui/material';
 import React from 'react';
+import InputOption from './InputOption';
 import "./Post.css";
+import ThumbUpAltOutlinedIcon from '@mui/icons-material/ThumbUpAltOutlined';
+import ChatOutlinedIcon from '@mui/icons-material/ChatOutlined';
+import ShareOutlinedIcon from '@mui/icons-material/ShareOutlined';
+import SendOutlinedIcon from "@mui/icons-material/SendOutlined"
 
 function Post({ name, description, message, photoUrl }) {
   return (
     <div className="post">
       <div className="post_header">
         <Avatar />
-        <div className="post_header">
-          <h2>Raunak Raj</h2>
-          <p>Description</p>
+        <div className="post_info">
+          <h2>{name}</h2>
+          <p>{description}</p>
         </div>
       </div>
       <div className="post_body">
-        <p>Message goes here</p>
+        <p>{message}</p>
+      </div>
+      <div className="post_buttons">
+        <InputOption Icon={ThumbUpAltOutlinedIcon} title="Like" color="gray" />
+        <InputOption Icon={ChatOutlinedIcon} title="Comment" color="gray" />
+        <InputOption Icon={ShareOutlinedIcon} title="Share" color="gray" />
+        <InputOption Icon={SendOutlinedIcon} title="Send" color="gray" />
       </div>
 
     </div>
