1f3115f3bfff02243d75062993c09d0cfb00b0ed
Rounak Raj Singh <97117309+RounakRajSingh620@users.noreply.github.com>
2023-03-07 22:43:49 +0530

    Linkedin-Clone

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase'
+const firebaseConfig = {
+    apiKey: "AIzaSyBBgOEV8h-rFOeWE1Phjh3HV_jkHR6PyXo",
+    authDomain: "linkedin-clone-a09ff.firebaseapp.com",
+    projectId: "linkedin-clone-a09ff",
+    storageBucket: "linkedin-clone-a09ff.appspot.com",
+    messagingSenderId: "504671310231",
+    appId: "1:504671310231:web:893fd7c6655805343db4d2",
+    measurementId: "G-0WWKGX84JH"
+  };
+  const firebaseApp=firebase.initializeApp(firebaseConfig);
