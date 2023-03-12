bbddd7624aade5f2df6b67aafdcc5c836cea30dd
Rounak Raj Singh <97117309+RounakRajSingh620@users.noreply.github.com>
2023-03-12 20:04:11 +0530

    Linkdein-Clone

---
+++ src/Login.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./Login.css";
+
+function Login() {
+    return (
+        <div className="Login">
+            <img src="https://news.hitb.org/sites/default/files/styles/large/public/field/image/500px-LinkedIn_Logo.svg__1.png?itok=q_lR0Vks" alt="" />
+            <form>
+            <input type="text"/></form>
+        </div>
+    )
+}
+
+export default Login
