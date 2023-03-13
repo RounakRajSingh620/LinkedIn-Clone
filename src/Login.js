901c7de154abb3fcacaf6ab308b161f19db48d80
Rounak Raj Singh <97117309+RounakRajSingh620@users.noreply.github.com>
2023-03-13 20:01:36 +0530

    Linkdein-Clone

--- src/Login.js
+++ src/Login.js
@@ -6,9 +6,11 @@ function Login() {
         <div className="Login">
             <img src="https://news.hitb.org/sites/default/files/styles/large/public/field/image/500px-LinkedIn_Logo.svg__1.png?itok=q_lR0Vks" alt="" />
             <form>
-            <input type="text"/></form>
+                <input placeholder='Full name(required if registrating'
+                    type="text" />
+            </form>
         </div>
-    )
+    );
 }
 
 export default Login
