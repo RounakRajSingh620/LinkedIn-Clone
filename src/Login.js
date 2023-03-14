33c9b40d3074a12361446f19a7e735da9b9caab5
Rounak Raj Singh <97117309+RounakRajSingh620@users.noreply.github.com>
2023-03-14 18:39:56 +0530

    Linkdein-Clone

--- src/Login.js
+++ src/Login.js
@@ -2,6 +2,7 @@ import React from 'react';
 import "./Login.css";
 
 function Login() {
+    const register=()=>{};
     return (
         <div className="Login">
             <img src="https://news.hitb.org/sites/default/files/styles/large/public/field/image/500px-LinkedIn_Logo.svg__1.png?itok=q_lR0Vks" alt="" />
@@ -9,9 +10,12 @@ function Login() {
                 <input placeholder='Full name(required if registrating'
                     type="text" />
                 <input placeholder='Profile pic URL (optional)' type="text" />
-                <input placeholder='Email' type="email" />
-                <input placeholder='Password' type="password" />
+                <input placeholder="Email" type="email" />
+                <input placeholder="Password" type="password" />
+                <button>Sign In</button>
             </form>
+            <p>Not a member ?
+                <span className="login_register" onClick={register}>Register Now</span></p>
         </div>
     );
 }
